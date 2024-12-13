577f02ee01a69989735fa5ee69f03e573c7dc63a
brandon <brandonmcphatter1@gmail.com>
2024-12-13 09:15:57 -0500

    moved NavBar.tsx to separate file in sections directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,23 @@
-import type { Metadata } from "next";
+import type {Metadata} from "next";
 import "./globals.css";
 import React from "react";
 
 
-
 export const metadata: Metadata = {
-  title: "Fall Coming",
-  description: "Music and Content from Brandon Jamall",
+    title: "Fall Coming",
+    description: "Music and Content from Brandon Jamall",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={'bg-blue-200 md:bg-yellow-200 lg:bg-green-200'}>
-      <NavBar />
-        {children}
-      </body>
-    </html>
-  );
-}
-
-export function NavBar() {
     return (
-        <nav className={'flex justify-center animate__animated animate__slideInDown animate__slower animate__delay-4s'}>Navigation</nav>
-    )
+        <html lang="en">
+        <body className={'bg-blue-200 md:bg-yellow-200 lg:bg-green-200'}>
+        {children}
+        </body>
+        </html>
+    );
 }
