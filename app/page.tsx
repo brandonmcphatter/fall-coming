9c7e5f5175e820226065d04e2b13386e813781de
brandon <brandonmcphatter1@gmail.com>
2024-12-15 05:00:46 -0500

    added music player component and removed footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,29 +2,35 @@ import HeroSection from "@/Sections/HeroSection/HeroSection";
 import MusicSection from "@/Sections/MusicSection/MusicSection";
 import BlogSection from "@/Sections/BlogSection/BlogSection";
 import NavBar from "@/Sections/HeroSection/NavBar";
+import MusicPlayerBar from "@/Sections/MusicSection/MusicPlayerBar";
 
 export default function Home() {
     return (
-        <div className={'mx-auto md:max-w-[85%] xl:max-w-[50%]'}>
+        <>
+            <div className={'mx-auto md:max-w-[85%] xl:max-w-[50%]'}>
 
-            <main className={'px-4 md:px-0 '}>
-                <NavBar/>
+                <main className={'px-4 md:px-0 '}>
+                    <NavBar/>
 
-                {/*Fall Coming Hero Section*/}
-                <HeroSection/>
+                    {/*Fall Coming Hero Section*/}
+                    <HeroSection/>
 
-                {/*Music Section*/}
-                <MusicSection/>
+                    {/*Music Section*/}
+                    <MusicSection/>
 
-                <BlogSection/>
+                    <BlogSection/>
 
-            </main>
+                </main>
 
-            <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center pb-6">
-                <p className={'text-sm'}>Footer 2025</p>
-            </footer>
 
-        </div>
+            </div>
+
+            <MusicPlayerBar/>
+
+            {/*<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center pb-6">*/}
+            {/*    <p className={'text-sm'}>Footer 2025</p>*/}
+            {/*</footer>*/}
+        </>
     );
 }
 
