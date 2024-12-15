f29baded4f347e45dde5590700b08e781631fd73
brandon <brandonmcphatter1@gmail.com>
2024-12-15 04:59:27 -0500

    added border on hover

--- Sections/BlogSection/BlogCard.tsx
+++ Sections/BlogSection/BlogCard.tsx
@@ -1,6 +1,6 @@
 export default function BlogCard() {
     return (
-        <div className={'blogCard flex justify-center items-center'}>
+        <div className={'blogCard flex justify-center items-center rounded cursor-pointer hover:border-2'}>
             <div>Blog Post</div>
         </div>
     )
