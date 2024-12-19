3c3a2034299aa3bcb72d74b79d57bf587b9c61d0
brandon <brandonmcphatter1@gmail.com>
2024-12-18 22:51:17 -0500

    cover art coming from supabase url

--- Sections/MusicSection/AlbumItem.tsx
+++ Sections/MusicSection/AlbumItem.tsx
@@ -10,7 +10,7 @@ export default function AlbumItem({cover, title}: AlbumItemProps ) {
         <div className={'rounded border hover:border-2'}>
             {/*Album Cover*/}
             <div className={'albumCover rounded'}>
-                <Image className={' hover:cursor-pointer'} src={cover? cover : 'https://picsum.photos/300'}
+                <Image className={' hover:cursor-pointer'} src={cover? cover : 'https://hpynsipfvbfvrjfldzjc.supabase.co/storage/v1/object/sign/Album%20Covers/FallComing2.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJBbGJ1bSBDb3ZlcnMvRmFsbENvbWluZzIuSlBHIiwiaWF0IjoxNzM0NTc3MTY4LCJleHAiOjE3NjYxMTMxNjh9.YjTQJ2m4WhDPvYnhfDxC4__s0xdQu_OKPjStYCQOsyU&t=2024-12-19T02%3A59%3A28.653Z'}
                        alt={'Album Cover'} fill={true}/>
             </div>
             {/*Album Info*/}
