cc48562feaedb9424fdb555aa79bfa01a0e32cd3
brandon <brandonmcphatter1@gmail.com>
2024-12-20 23:56:49 -0500

    condensed albumItems into single component (albumGrid)

--- Sections/MusicSection/MusicSection.tsx
+++ Sections/MusicSection/MusicSection.tsx
@@ -1,25 +1,18 @@
-import AlbumItem from "@/Sections/MusicSection/AlbumItem";
+import AlbumGrid from "@/Sections/MusicSection/AlbumGrid";
+import SectionTitle from "@/Sections/SectionTitle";
 
 export default function MusicSection() {
+
     return (
         <section id={'music'} className={'px-4 md:px0 py-8 min-h-fit'}>
 
             {/*Section Title*/}
-            <div className={'text-5xl mb-8'}>Music</div>
+            <SectionTitle title={'Music'}/>
 
             {/*Featured Section*/}
 
             {/*Grid of Albums*/}
-            <div className={'grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 md:gap-6'}>
-                <AlbumItem />
-                <AlbumItem />
-                <AlbumItem />
-                <AlbumItem />
-                <AlbumItem />
-                <AlbumItem />
-
-            </div>
+            <AlbumGrid />
         </section>
     )
 }
-
