d7d0ec4d0b3bf5bfea31a54c1fb83a6dc90c5213
brandon <brandonmcphatter1@gmail.com>
2024-12-20 23:58:13 -0500

    removed blog section and music player to focus on music section and page functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import HeroSection from "@/Sections/HeroSection/HeroSection";
 import MusicSection from "@/Sections/MusicSection/MusicSection";
-import BlogSection from "@/Sections/BlogSection/BlogSection";
 import NavBar from "@/Sections/HeroSection/NavBar";
-import MusicPlayerBar from "@/Sections/MusicSection/MusicPlayerBar";
 
 export default function Home() {
+
     return (
         <>
             <div className={'mx-auto md:max-w-[85%] xl:max-w-[50%]'}>
 
-                <main className={'px-4 md:px-0 '}>
+                <main className={'px-4 md:px-0 mb-20'}>
                     <NavBar/>
 
                     {/*Fall Coming Hero Section*/}
@@ -18,14 +17,14 @@ export default function Home() {
                     {/*Music Section*/}
                     <MusicSection/>
 
-                    <BlogSection/>
+                    {/*<BlogSection/>*/}
 
                 </main>
 
 
             </div>
 
-            <MusicPlayerBar/>
+            {/*<MusicPlayerBar/>*/}
 
             {/*<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center pb-6">*/}
             {/*    <p className={'text-sm'}>Footer 2025</p>*/}
