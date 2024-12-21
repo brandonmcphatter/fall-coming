137cef42396ef3d910ca4fe9794654060cccf226
brandon <brandonmcphatter1@gmail.com>
2024-12-20 23:55:44 -0500

    links to /music

--- Sections/MusicSection/AlbumItem.tsx
+++ Sections/MusicSection/AlbumItem.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 type AlbumItemProps = {
     cover?: string;
@@ -7,16 +8,19 @@ type AlbumItemProps = {
 
 export default function AlbumItem({cover, title}: AlbumItemProps ) {
     return (
-        <div className={'rounded border hover:border-2'}>
-            {/*Album Cover*/}
-            <div className={'albumCover rounded'}>
-                <Image className={' hover:cursor-pointer'} src={cover? cover : 'https://hpynsipfvbfvrjfldzjc.supabase.co/storage/v1/object/sign/Album%20Covers/FallComing2.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJBbGJ1bSBDb3ZlcnMvRmFsbENvbWluZzIuSlBHIiwiaWF0IjoxNzM0NTc3MTY4LCJleHAiOjE3NjYxMTMxNjh9.YjTQJ2m4WhDPvYnhfDxC4__s0xdQu_OKPjStYCQOsyU&t=2024-12-19T02%3A59%3A28.653Z'}
-                       alt={'Album Cover'} fill={true}/>
+        <Link href={'/music'}>
+            <div>
+                {/*Album Cover*/}
+                <div className={'albumCover rounded  overflow-hidden'}>
+                    <Image className={' hover:cursor-pointer'}
+                           src={cover ? cover : 'https://hpynsipfvbfvrjfldzjc.supabase.co/storage/v1/object/sign/Album%20Covers/FallComing2.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJBbGJ1bSBDb3ZlcnMvRmFsbENvbWluZzIuSlBHIiwiaWF0IjoxNzM0NTc3MTY4LCJleHAiOjE3NjYxMTMxNjh9.YjTQJ2m4WhDPvYnhfDxC4__s0xdQu_OKPjStYCQOsyU&t=2024-12-19T02%3A59%3A28.653Z'}
+                           alt={'Album Cover'} fill={true}/>
+                </div>
+                {/*Album Info*/}
+                <div className={'flex justify-between ps-1 w-fit'}>
+                    <span className={'hover:cursor-pointer text-sm text-gray-300'}>{title}</span>
+                </div>
             </div>
-            {/*Album Info*/}
-            <div className={'flex justify-between ps-1 w-fit'}>
-                <span className={'hover:cursor-pointer'}>{title}</span>
-            </div>
-        </div>
+        </Link>
     )
 }
