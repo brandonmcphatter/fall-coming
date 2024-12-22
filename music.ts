bb5a8c043e3984cff4c24b4dd37b8d91cbc66d66
brandon <brandonmcphatter1@gmail.com>
2024-12-21 23:20:02 -0500

    changed to ts file

---
+++ music.ts
@@ -0,0 +1,37 @@
+export const albums: ({ title: string; tracks: string[]; cover?: string; songLengths: number[]; artist: string })[] = [
+    {
+        title: 'Fall Coming 2',
+        tracks: ['Reminisce', 'Space', 'Wasted', 'Wasted II', 'Maybe Not Freestyle', 'Mirror'],
+        cover: 'https://hpynsipfvbfvrjfldzjc.supabase.co/storage/v1/object/sign/Album%20Covers/FallComing2.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJBbGJ1bSBDb3ZlcnMvRmFsbENvbWluZzIuSlBHIiwiaWF0IjoxNzM0NzM0MzczLCJleHAiOjE3NjYyNzAzNzN9.R11-M6CvaKPyguIuBcMUFhoaRR8XzJCtsjtGtViSCCI&t=2024-12-20T22%3A39%3A33.905Z',
+        songLengths: [65, 124, 127, 142, 123, 196],
+        artist: 'Brandon Jamall'
+    },
+    {
+        title: 'Energy',
+        tracks: ['Fever', 'Mood Swang', 'Disagree'],
+        cover: 'https://hpynsipfvbfvrjfldzjc.supabase.co/storage/v1/object/sign/Album%20Covers/EnergyCover.jpeg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJBbGJ1bSBDb3ZlcnMvRW5lcmd5Q292ZXIuanBlZyIsImlhdCI6MTczNDczNDU1OSwiZXhwIjoxNzY2MjcwNTU5fQ.XQO2qsl4m0ysChonhtQJR8nMD3rZ_m6h0k0t3Jq6wvs&t=2024-12-20T22%3A42%3A39.508Z',
+        songLengths: [216, 226, 228],
+        artist: 'Brandon Jamall & Jamien Pride'
+    },
+    {
+        title: 'On Read',
+        tracks: ['On Read'],
+        cover: 'https://hpynsipfvbfvrjfldzjc.supabase.co/storage/v1/object/sign/Album%20Covers/OnReadCover.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJBbGJ1bSBDb3ZlcnMvT25SZWFkQ292ZXIuanBnIiwiaWF0IjoxNzM0NzM0NjI4LCJleHAiOjE3NjYyNzA2Mjh9.Rt7biiws17sCGbqEl5l22PpbugJToQB9l-_CD0lNJAc&t=2024-12-20T22%3A43%3A48.776Z',
+        songLengths: [190],
+        artist: 'Brandon Jamall'
+    },
+    {
+        title: 'Run It Up',
+        tracks: ['Run It Up'],
+        cover: 'https://hpynsipfvbfvrjfldzjc.supabase.co/storage/v1/object/sign/Album%20Covers/RunItUpCover2.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJBbGJ1bSBDb3ZlcnMvUnVuSXRVcENvdmVyMi5KUEciLCJpYXQiOjE3MzQ3MzQ3MzcsImV4cCI6MTc2NjI3MDczN30.NQfKxsTWMCgOF2Uq-Sw-y5ooskiGNPA2FsC4BtCmXkI&t=2024-12-20T22%3A45%3A37.250Z',
+        songLengths: [138],
+        artist: 'Brandon Jamall'
+    },
+    {
+        title: 'Fall Coming',
+        tracks: ['Someone', 'Right Now', 'Hi & Lo'],
+        // missing cover
+        songLengths: [179, 149, 195],
+        artist: 'Brandon Jamall'
+    }
+];
