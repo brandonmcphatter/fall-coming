bb56040823a3042604dcba9301939b4181d0fc20
brandon <brandonmcphatter1@gmail.com>
2024-12-22 01:10:13 -0500

    removed useContext

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 
 import type {Metadata} from "next";
 import "./globals.css";
-import React, {useContext} from "react";
-import {AlbumContext, AlbumProvider} from "@/context/AlbumContext";
+import {AlbumProvider} from "@/context/AlbumContext";
 
 export const metadata: Metadata = {
     title: "Fall Coming",
