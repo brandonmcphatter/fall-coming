df21245767b6c420424d4f34097581aaa1d3aef3
brandon <brandonmcphatter1@gmail.com>
2024-12-22 01:08:13 -0500

    new route - empty for now

---
+++ app/contribute/page.tsx
@@ -0,0 +1,16 @@
+export default function ContributePage() {
+    return (
+        <>
+            <div className={'mx-auto md:max-w-[85%] xl:max-w-[50%] min-h-screen'}>
+
+                <main className={'px-4 md:px-0 mb-20'}>
+
+                    {/*All Music*/}
+                    <div>Contribute</div>
+
+                </main>
+
+            </div>
+        </>
+    )
+}
---
+++ app/merch/page.tsx
@@ -0,0 +1,16 @@
+export default function MerchPage() {
+    return (
+        <>
+            <div className={'mx-auto md:max-w-[85%] xl:max-w-[50%] min-h-screen'}>
+
+                <main className={'px-4 md:px-0 mb-20'}>
+
+                    {/*All Music*/}
+                    <div>Merch</div>
+
+                </main>
+
+            </div>
+        </>
+    )
+}
