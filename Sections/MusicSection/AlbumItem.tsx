a040669b2bb2a26048b92b807ded797066a6dfb2
brandon <brandonmcphatter1@gmail.com>
2024-12-21 23:22:37 -0500

    used context to change album choice

--- Sections/MusicSection/AlbumItem.tsx
+++ Sections/MusicSection/AlbumItem.tsx
@@ -1,17 +1,25 @@
+'use client'
+
 import Image from "next/image";
 import Link from "next/link";
+import {AlbumContext} from "@/context/AlbumContext";
+import {useContext} from "react";
 
 type AlbumItemProps = {
     cover?: string;
     title?: string;
+    albumId: number;
 }
 
-export default function AlbumItem({cover, title}: AlbumItemProps ) {
+export default function AlbumItem({cover, title, albumId}: AlbumItemProps ) {
+
+    const {setAlbumChoice} = useContext(AlbumContext);
+
     return (
         <Link href={'/music'}>
             <div>
                 {/*Album Cover*/}
-                <div className={'albumCover rounded  overflow-hidden'}>
+                <div className={'albumCover rounded  overflow-hidden'} onClick={()=>setAlbumChoice(albumId)}>
                     <Image className={' hover:cursor-pointer'}
                            src={cover ? cover : 'https://hpynsipfvbfvrjfldzjc.supabase.co/storage/v1/object/sign/Album%20Covers/FallComing2.JPG?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJBbGJ1bSBDb3ZlcnMvRmFsbENvbWluZzIuSlBHIiwiaWF0IjoxNzM0NTc3MTY4LCJleHAiOjE3NjYxMTMxNjh9.YjTQJ2m4WhDPvYnhfDxC4__s0xdQu_OKPjStYCQOsyU&t=2024-12-19T02%3A59%3A28.653Z'}
                            alt={'Album Cover'} fill={true}/>
