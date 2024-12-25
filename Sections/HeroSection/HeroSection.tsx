4c88c694d0b9f12e20dbd3ffe65a557d4a437497
brandon <brandonmcphatter1@gmail.com>
2024-12-25 04:19:16 -0500

    added silkscreen font to 'coming' in .coming css class

--- Sections/HeroSection/HeroSection.tsx
+++ Sections/HeroSection/HeroSection.tsx
@@ -2,22 +2,22 @@ import 'animate.css';
 
 export default function HeroSection() {
     return (
-        <section id={'hero'} className={'h-svh flex-col justify-center  content-center items-stretch'}>
+        <section id={'hero'} className={'h-screen flex-col justify-center  content-center '}>
 
             {/*Main Text*/}
-            <div className={' flex flex-col lg:flex-row justify-center text-8xl mx-4 mb-3'}>
+            <div className={' flex flex-col lg:flex-row justify-center text-8xl mx-4 mb-3 '}>
 
                 <div className={'text-center lg:pe-6 md:pe-72 animate__animated animate__fadeInDownBig animate__slower'}>
                     Fall
                 </div>
-                <div className={'text-center tracking-wider md:ps-20 lg:ps-0 animate__animated animate__fadeIn animate__slower animate__delay-3s'} >
-                   <span className={''}> Coming</span>
+                <div className={'text-center tracking-wide md:ps-20 lg:ps-0 animate__animated animate__fadeIn animate__slower animate__delay-3s'} >
+                   <span className={'coming'}> Coming</span>
                 </div>
 
             </div>
 
             {/*Sub Text*/}
-            <p className={'text-center italic tracking-wide animate__animated animate__zoomInUp animate__slower animate__delay-3s'}>The Latest Content & Creations From Brandon Jamall</p>
+            <p className={'text-center italic tracking-wide animate__animated animate__zoomInUp animate__slower animate__delay-3s mb-40'}>The Latest Content & Creations From Brandon Jamall</p>
 
         </section>
     );
