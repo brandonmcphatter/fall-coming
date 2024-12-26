c6d3f1d642518281d398bc87d494dd324be963d1
brandon <brandonmcphatter1@gmail.com>
2024-12-26 17:09:27 -0500

    timeline functionality complete

--- Sections/MusicSection/MusicPlayerBar.tsx
+++ Sections/MusicSection/MusicPlayerBar.tsx
@@ -8,35 +8,48 @@ import {NextButton, PauseButton, PlayButton, PrevButton} from "@/Sections/MusicS
 
 export default function MusicPlayerBar() {
 
+    // context
+    const {currentSong, formatTime, playing, setPlaying, trackUrl} = useContext(AlbumContext);
 
-    const [currentTime, setCurrentTime] = useState<number>(0);
-    const audioRef = useRef<HTMLAudioElement | null>(null);
-    const trackUrl = 'https://hpynsipfvbfvrjfldzjc.supabase.co/storage/v1/object/sign/Album%20Covers/reminisce.mp3?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJBbGJ1bSBDb3ZlcnMvcmVtaW5pc2NlLm1wMyIsImlhdCI6MTczNTE4MTk3OCwiZXhwIjoxNzY2NzE3OTc4fQ.zX82e2QnLtYjCXmRFYdN7SxUILxpJ3v6xi4v2MWwu_g&t=2024-12-26T02%3A59%3A38.416Z'
-    const {currentSong, formatTime, playing, setPlaying} = useContext(AlbumContext);
+    const audioRef = useRef<HTMLAudioElement | null>(trackUrl ? new Audio(trackUrl) : null);
+    const [currentTime, setCurrentTime] = useState<number>(audioRef.current?.currentTime || 0);
 
 
-    // Pause + Play Function
+    // Pause + Play Functionality
     const togglePlay = () => {
-        if (audioRef.current){
+        if (audioRef.current) {
             if (playing) {
                 audioRef.current.pause();
             } else {
                 audioRef.current.play();
             }
+
         }
         setPlaying(!playing);
     }
 
+    const updateCurrentTime = () => {
+        if (audioRef.current) {
+            setCurrentTime(Math.floor(audioRef.current.currentTime));
+        }
+    }
     const handleSeek = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (currentSong?.songDuration) {
             const newTime = Math.floor((Number(e.target.value)));
             setCurrentTime(newTime)
+            if (audioRef.current) audioRef.current.currentTime = newTime;
         }
+    }
+
+
+    if (audioRef.current && audioRef.current.ended) {
+        setPlaying(false);
+
     }
 
     return (
         <div className={'musicPlayerBar animate__animated animate__slideInUp '}>
-            <audio ref={audioRef} src={trackUrl} autoPlay={true}>Not Supported</audio>
+            <audio ref={audioRef} src={trackUrl} autoPlay={true} onTimeUpdate={updateCurrentTime}>Not Supported</audio>
             <div className={' flex justify-between items-center '}>
                 {/*Cover Art & Title*/}
                 <div className={'flex items-center gap-3'}>
@@ -48,17 +61,17 @@ export default function MusicPlayerBar() {
                 {/*Music Controls*/}
                 <div className={'flex justify-center gap-2.5 md:gap-3 lg:gap-4'}>
                     <div><PrevButton/></div>
-                    <div onClick={togglePlay}>{playing ? <PlayButton/> : <PauseButton/>}</div>
+                    <div onClick={togglePlay}>{playing ? <PauseButton/> : <PlayButton/>}</div>
                     <div><NextButton/></div>
                 </div>
             </div>
 
             {/*Time Bar*/}
             <div className={'flex gap-3 text-center'}>
-                <div>{formatTime(currentTime)}</div>
-                <input type="range" step={1} defaultValue={currentTime} min={0} max={currentSong?.songDuration}
+                <div>{formatTime(currentTime) || 0}</div>
+                <input type="range" step={1} value={currentTime} min={0} max={currentSong?.songDuration}
                        className={'w-full cursor-pointer'} onChange={handleSeek}/>
-                <div>{formatTime(currentSong?.songDuration)}</div>
+                <div>{currentSong?.songDuration && formatTime(currentSong?.songDuration - currentTime)}</div>
             </div>
 
         </div>
