1d64846bb58b531c514520d2f28731f8daad10ce
brandon <brandonmcphatter1@gmail.com>
2024-12-25 21:08:13 -0500

    fall coming changed completely to silkscreen font - subtitle uses fall class

--- Sections/HeroSection/HeroSection.tsx
+++ Sections/HeroSection/HeroSection.tsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
             <div className={' flex flex-col lg:flex-row justify-center text-8xl mx-4 mb-3 '}>
 
                 <div className={'text-center lg:pe-6 md:pe-72 animate__animated animate__fadeInDownBig animate__slower'}>
-                    Fall
+                    <span className={'coming'}>Fall</span>
                 </div>
                 <div className={'text-center tracking-wide md:ps-20 lg:ps-0 animate__animated animate__fadeIn animate__slower animate__delay-3s'} >
                    <span className={'coming'}> Coming</span>
@@ -17,7 +17,7 @@ export default function HeroSection() {
             </div>
 
             {/*Sub Text*/}
-            <p className={'text-center italic tracking-wide animate__animated animate__zoomInUp animate__slower animate__delay-3s mb-40'}>The Latest Content & Creations From Brandon Jamall</p>
+            <p className={'text-center italic tracking-wide animate__animated animate__zoomInUp animate__slower animate__delay-3s mb-40 fall'}>The Latest Content & Creations From Brandon Jamall</p>
 
         </section>
     );
