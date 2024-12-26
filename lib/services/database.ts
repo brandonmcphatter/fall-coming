b0a3a7622ca855a222482c244b574ffab57fc3e0
brandon <brandonmcphatter1@gmail.com>
2024-12-26 17:06:20 -0500

    new function to fetch song url

--- lib/services/database.ts
+++ lib/services/database.ts
@@ -10,3 +10,17 @@ export async function fetchAllMusic() {
     }
     return data;
 }
+
+export async function fetchSongUrl(songTitle: string) {
+    const {data, error} = await supabase
+        .from('Music')
+        .select('track_url').eq('song_title', songTitle).single();
+    if (error) {
+        console.error('Error fetching song data:', error.message);
+        throw error;
+    }
+    if (!data) {
+        throw new Error('Song not found');
+    }
+    return data;
+}
