7cd0895bc8672f62b011ca6b88750447b7ec558d
brandon <brandonmcphatter1@gmail.com>
2024-12-25 23:35:58 -0500

    added util functions / state

--- context/AlbumContext.tsx
+++ context/AlbumContext.tsx
@@ -13,6 +13,9 @@ type AlbumContextType = {
     formatTime: (duration: number | undefined) => string;
     currentSong: { title: string, coverArt: string | undefined, songDuration: number } | null;
     setCurrentSong: (song: { title: string; coverArt: string | undefined; songDuration: number }) => void;
+    getAudioDuration: (audioUrl: string) => Promise<number>;
+    playing: boolean;
+    setPlaying: (playing: boolean) => void;
 };
 
 export const AlbumContext = createContext<AlbumContextType>({} as AlbumContextType);
@@ -22,6 +25,7 @@ export function AlbumProvider({children}: { children: ReactNode }) {
     const currentAlbum = albums[albumChoice] || albums[0];
     const allAlbums = albums;
     const [showMusicPlayer, setShowMusicPlayer] = useState<boolean>(false);
+    const [playing, setPlaying] = useState<boolean>(true);
     const formatTime = (duration: number | undefined) => {
         if (duration === undefined) return '0:00';
         const minutes = Math.floor(duration / 60);
@@ -34,6 +38,25 @@ export function AlbumProvider({children}: { children: ReactNode }) {
         songDuration: number
     } | null>(null);
 
+    // Gets duration of selected audio
+    const getAudioDuration = async (audioUrl: string): Promise<number> => {
+        return new Promise((resolve, reject) => {
+            const audio = new Audio(audioUrl);
+
+            // Event triggered when the metadata is loaded
+            audio.addEventListener('loadedmetadata', () => {
+                resolve(audio.duration); // Returns the duration in seconds
+            });
+
+            // Handle errors
+            audio.addEventListener('error', () => {
+                reject(new Error('Failed to load audio metadata'));
+            });
+        });
+    };
+
+
+
     return (
         <AlbumContext.Provider value={{
             albumChoice,
@@ -44,7 +67,10 @@ export function AlbumProvider({children}: { children: ReactNode }) {
             showMusicPlayer,
             setShowMusicPlayer,
             currentSong,
-            setCurrentSong
+            setCurrentSong,
+            getAudioDuration,
+            playing,
+            setPlaying,
         }}>
             {children}
         </AlbumContext.Provider>
