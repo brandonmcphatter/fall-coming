bf32a1b222a48e0269335c1eed223a8b5fc8bc6a
brandon <brandonmcphatter1@gmail.com>
2024-12-25 23:36:43 -0500

    added home icon to mobile nav and linked it

--- Sections/RegularNav.tsx
+++ Sections/RegularNav.tsx
@@ -10,14 +10,27 @@ export default function RegularNav() {
     return (
         <nav
             className={' px-4 py-4 md:py-6 md:px-0 mx-auto relative'}>
-            <div className={'w-full md:hidden flex flex-col items-stretch'}>
+            <div className={' w-full md:hidden flex flex-col'}>
                 {/*Hamburger Icon*/}
-                <div className={' md:hidden cursor-pointer self-end pe-4'} onClick={() => setShow(!show)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                         stroke="currentColor" className="size-10">
-                        <path strokeLinecap="round" strokeLinejoin="round"
-                              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
-                    </svg>
+                <div className={' md:hidden cursor-pointer flex justify-between'} >
+                    <Link href={'/'}>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                             className="size-10">
+                            <path
+                                d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z"/>
+                            <path
+                                d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z"/>
+                        </svg>
+                    </Link>
+
+                        <svg onClick={() => setShow(!show)} xmlns="http://www.w3.org/2000/svg" fill="none"
+                             viewBox="0 0 24 24" strokeWidth="1.5"
+                             stroke="currentColor" className="size-10">
+                            <path strokeLinecap="round" strokeLinejoin="round"
+                                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                        </svg>
+
+
                 </div>
 
                 {/*Mobile Modal*/}
