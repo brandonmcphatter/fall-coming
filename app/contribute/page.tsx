c20c6c338eee437b37f28170abeb91f86c824226
brandon <brandonmcphatter1@gmail.com>
2025-01-02 23:40:36 -0500

    starting design for contribute page

--- app/contribute/page.tsx
+++ app/contribute/page.tsx
@@ -5,8 +5,16 @@ export default function ContributePage() {
 
                 <main className={'px-4 md:px-0 mb-20'}>
 
-                    {/*All Music*/}
-                    <div>Contribute</div>
+                    <section>
+                        <div className={'text-center'}>Contribute</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium adipisci, amet
+                            doloremque earum error labore magni molestiae mollitia non quae quis rem sapiente sit
+                            tempore ullam unde. Animi commodi magnam minus tempore unde, vero voluptates! Deserunt
+                            dolor, doloribus id illum incidunt, laboriosam molestias nam praesentium reprehenderit saepe
+                            sequi suscipit.
+                        </p>
+
+                    </section>
 
                 </main>
 
