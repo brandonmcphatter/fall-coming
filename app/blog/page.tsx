18a1f474325d5f2a422d7524fe2405f6679e043c
brandon <brandonmcphatter1@gmail.com>
2025-01-03 23:10:35 -0500

    page created / needs to be filled

---
+++ app/blog/page.tsx
@@ -0,0 +1,17 @@
+import SectionTitle from "@/Sections/SectionTitle";
+
+export default function BlogPage() {
+    return (
+        <>
+            <div className={'mx-auto md:max-w-[85%] xl:max-w-[50%] min-h-screen'}>
+
+                <main className={'px-4 md:px-0 mb-20 h-svh flex justify-center items-center'}>
+
+                    <SectionTitle title={'Coming Soon'} />
+
+                </main>
+
+            </div>
+        </>
+    );
+}
