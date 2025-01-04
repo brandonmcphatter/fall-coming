6288f88bd9f4c0ba95dcb761f3bded8cf71f8ea3
brandon <brandonmcphatter1@gmail.com>
2025-01-04 12:07:09 -0500

    added underlining and fixed display on modal / changed font

---
+++ components/HeroSection/NavBar.tsx
@@ -0,0 +1,141 @@
+'use client';
+
+import {useState} from "react";
+import Link from "next/link";
+import {
+    HamburgerIcon,
+    HomeIcon,
+    SmallBlogIcon, SmallContributeIcon,
+    SmallHomeIcon,
+    SmallMerchIcon,
+    SmallMusicIcon
+} from "@/components/Icons";
+
+export default function NavBar() {
+
+    const [show, setShow] = useState<boolean>(false);
+    const closeMenu = () => setShow(false);
+    const changeShow = () => setShow(!show);
+
+    return (
+        <>
+        <nav className={' navBlur px-8 py-8 md:py-6 md:px-0 mx-auto animate__animated animate__slideInDown animate__slower animate__delay-4s fixed top-0 w-full z-50'}>
+            <div className={' w-full md:hidden flex flex-col'}>
+                <div className={' md:hidden cursor-pointer flex justify-between'}>
+                    <Link href={'/'} onClick={closeMenu}>
+                        <HomeIcon />
+                    </Link>
+
+                    <div onClick={changeShow}>
+                        <HamburgerIcon />
+                    </div>
+
+
+                </div>
+
+                {/*Mobile Modal*/}
+                {show &&
+                    <div className={'coming z-50 h-svh flex justify-center items-center pb-44 sticky modal'}>
+                        <ul className={'list-none flex flex-col gap-8 absolute'}>
+                            <Link href={'/'}>
+                                <li className={'navItem hover:underline'} onClick={closeMenu}>
+                                <span>Home</span>
+                                    <SmallHomeIcon />
+                                </li>
+                            </Link>
+                            <Link href={'/music'}>
+                                <li className={'navItem hover:underline'} onClick={closeMenu}>
+                                    <span>Music</span>
+                                    <SmallMusicIcon/>
+                                </li>
+                            </Link>
+                            <Link href={'/blog'}>
+                                <li className={'navItem hover:underline'} onClick={closeMenu}>
+                                    <span>Blog</span>
+                                    <SmallBlogIcon/>
+                                </li>
+                            </Link>
+                            <Link href={'/merch'}>
+                                <li className={'navItem hover:underline'} onClick={closeMenu}>
+                                    <span>Merch</span>
+                                    <SmallMerchIcon/>
+                                </li>
+                            </Link>
+                            <Link href={'/contribute'}>
+                                <li className={'navItem hover:underline'} onClick={closeMenu}>
+                                    <span>Contribute</span>
+                                    <SmallContributeIcon/>
+                                </li>
+                            </Link>
+                        </ul>
+                    </div>
+                }
+            </div>
+
+            {/*Medium Screen Nav Links*/}
+            <ul className={'coming w-full hidden md:flex justify-evenly'}>
+                <Link href={'/'}>
+                    <li className={'navItem'}>
+                        <span>Home</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                             className="size-6">
+                            <path
+                                d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z"/>
+                            <path
+                                d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z"/>
+                        </svg>
+                    </li>
+                </Link>
+                <Link href={'/music'}>
+                    <li className={'navItem'}>
+                        <span>Music</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                             className="size-6">
+                            <path fillRule="evenodd"
+                                  d="M19.952 1.651a.75.75 0 0 1 .298.599V16.303a3 3 0 0 1-2.176 2.884l-1.32.377a2.553 2.553 0 1 1-1.403-4.909l2.311-.66a1.5 1.5 0 0 0 1.088-1.442V6.994l-9 2.572v9.737a3 3 0 0 1-2.176 2.884l-1.32.377a2.553 2.553 0 1 1-1.402-4.909l2.31-.66a1.5 1.5 0 0 0 1.088-1.442V5.25a.75.75 0 0 1 .544-.721l10.5-3a.75.75 0 0 1 .658.122Z"
+                                  clipRule="evenodd"/>
+                        </svg>
+                    </li>
+                </Link>
+                <Link href={'/blog'}>
+                    <li className={'navItem'}>
+                        <span>Blog</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                             className="size-6">
+                            <path
+                                d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z"/>
+                            <path
+                                d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z"/>
+                        </svg>
+
+                    </li>
+                </Link>
+                <Link href={'/merch'}>
+                    <li className={'navItem'}>
+                        <span>Merch</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                             className="size-6">
+                            <path
+                                d="M2.25 2.25a.75.75 0 0 0 0 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 0 0-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 0 0 0-1.5H5.378A2.25 2.25 0 0 1 7.5 15h11.218a.75.75 0 0 0 .674-.421 60.358 60.358 0 0 0 2.96-7.228.75.75 0 0 0-.525-.965A60.864 60.864 0 0 0 5.68 4.509l-.232-.867A1.875 1.875 0 0 0 3.636 2.25H2.25ZM3.75 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0ZM16.5 20.25a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0Z"/>
+                        </svg>
+
+                    </li>
+                </Link>
+                <Link href={'/contribute'}>
+                    <li className={'navItem'}>
+                        <span>Contribute</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                             className="size-6">
+                            <path
+                                d="M10.464 8.746c.227-.18.497-.311.786-.394v2.795a2.252 2.252 0 0 1-.786-.393c-.394-.313-.546-.681-.546-1.004 0-.323.152-.691.546-1.004ZM12.75 15.662v-2.824c.347.085.664.228.921.421.427.32.579.686.579.991 0 .305-.152.671-.579.991a2.534 2.534 0 0 1-.921.42Z"/>
+                            <path fillRule="evenodd"
+                                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v.816a3.836 3.836 0 0 0-1.72.756c-.712.566-1.112 1.35-1.112 2.178 0 .829.4 1.612 1.113 2.178.502.4 1.102.647 1.719.756v2.978a2.536 2.536 0 0 1-.921-.421l-.879-.66a.75.75 0 0 0-.9 1.2l.879.66c.533.4 1.169.645 1.821.75V18a.75.75 0 0 0 1.5 0v-.81a4.124 4.124 0 0 0 1.821-.749c.745-.559 1.179-1.344 1.179-2.191 0-.847-.434-1.632-1.179-2.191a4.122 4.122 0 0 0-1.821-.75V8.354c.29.082.559.213.786.393l.415.33a.75.75 0 0 0 .933-1.175l-.415-.33a3.836 3.836 0 0 0-1.719-.755V6Z"
+                                  clipRule="evenodd"/>
+                        </svg>
+                    </li>
+                </Link>
+            </ul>
+        </nav>
+        </>
+    )
+}
