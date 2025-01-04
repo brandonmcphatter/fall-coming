ee1ec24fb795d3a1ba1a8012becb4058c9dfcc49
brandon <brandonmcphatter1@gmail.com>
2025-01-04 12:10:31 -0500

    changed side margin to padding added bottom margin and changed max width on xl

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import type {Metadata} from "next";
 import "./globals.css";
 import {AlbumProvider} from "@/context/AlbumContext";
-import NavBar from "@/Sections/HeroSection/NavBar";
+import NavBar from "@/components/HeroSection/NavBar";
 
 export const metadata: Metadata = {
     title: "Fall Coming",
@@ -18,7 +18,7 @@ export default function RootLayout({
         <AlbumProvider>
         <html lang="en">
         <body>
-        <div className={'mx-auto md:max-w-[85%] xl:max-w-[60%]'}>
+        <div className={'px-auto md:max-w-[85%] xl:max-w-[30%] mb-24'}>
             <NavBar/>
         </div>
             {children}
