16ac5d4ee0486185525d512c597237f7d1a14b20
brandon <brandonmcphatter1@gmail.com>
2025-01-03 23:12:01 -0500

    section created for homepage

---
+++ Sections/MerchSection/MerchSection.tsx
@@ -0,0 +1,18 @@
+import SectionTitle from "@/Sections/SectionTitle";
+import Link from "next/link";
+
+export default function MerchSection() {
+    return (
+        <section id={'music'} className={'px-4 md:px0 py-8 min-h-[300px]'}>
+
+            {/*Section Title*/}
+            <Link href={'/merch'}>
+                <SectionTitle title={'Merch'}/>
+            </Link>
+            <div className={'flex justify-center items-center h-40'}>
+                <p className={'coming '}>Coming Soon</p>
+            </div>
+
+        </section>
+    )
+}
