bf13d268396458670f07fd8abb3c8d632f863bef
brandon <brandonmcphatter1@gmail.com>
2025-01-03 23:10:24 -0500

    page created / needs to be filled

--- app/contribute/page.tsx
+++ app/contribute/page.tsx
@@ -1,20 +1,12 @@
+import SectionTitle from "@/Sections/SectionTitle";
+
 export default function ContributePage() {
     return (
         <>
             <div className={'mx-auto md:max-w-[85%] xl:max-w-[50%] min-h-screen'}>
+                <main className={'px-4 md:px-0 mb-20 h-svh flex justify-center items-center'}>
 
-                <main className={'px-4 md:px-0 mb-20'}>
-
-                    <section>
-                        <div className={'text-center'}>Contribute</div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab accusantium adipisci, amet
-                            doloremque earum error labore magni molestiae mollitia non quae quis rem sapiente sit
-                            tempore ullam unde. Animi commodi magnam minus tempore unde, vero voluptates! Deserunt
-                            dolor, doloribus id illum incidunt, laboriosam molestias nam praesentium reprehenderit saepe
-                            sequi suscipit.
-                        </p>
-
-                    </section>
+                    <SectionTitle title={'Coming Soon'} />
 
                 </main>
 
--- app/merch/page.tsx
+++ app/merch/page.tsx
@@ -3,10 +3,9 @@ export default function MerchPage() {
         <>
             <div className={'mx-auto md:max-w-[85%] xl:max-w-[50%] min-h-screen'}>
 
-                <main className={'px-4 md:px-0 mb-20'}>
+                <main className={'px-4 md:px-0 mb-20 h-svh flex justify-center items-center'}>
 
-                    {/*All Music*/}
-                    <div>Merch</div>
+                    <div className={'coming text-4xl'}>Coming Soon</div>
 
                 </main>
 
